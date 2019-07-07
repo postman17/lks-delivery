34d29d4125ec3fe1671e1a1ea00f230152a5caa4
postman17 <frompostal@gmail.com>
2019-07-07 19:14:10 +0500

    change dockerdile and entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,5 @@ set -x
 
 echo "Start gunicorn"
 
-gunicorn -b 0.0.0.0:5442 app:app --worker-class aiohttp.GunicornWebWorker
+gunicorn -b 0.0.0.0:5442 app:app --worker-class aiohttp.GunicornWebWorker --access-logfile /access.log --error-logfile /error.log --log-level "warning"
 
-echo "End"
